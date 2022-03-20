1505a22d2fc2b750d6b93d51d57e4811c5f2ae38
mariniou77 <antonis.marinidis7@gmail.com>
2022-03-20 22:10:37 +0100

    first tasks of exercise 1

--- Exercise_1/task_2_1.py
+++ Exercise_1/task_2_1.py
@@ -1,4 +1,17 @@
-dt = open("datasets/ratings.csv", "r")
-print(dt.read())
+# @author Antonis Marinidis
+dt_list = []
 
-dt.close()
+# opening and reading the file withe the ratings
+dataset = "Exercise_1/datasets/ratings.csv"
+with open(dataset) as d:
+    lines = d.readlines()
+
+# extract the 3rd column which is the rating and store it in the list after converting it to float
+for rate in lines:
+    rating = rate.split(",")
+    if rating[2] != "rating":
+        dt_list.append(float(rating[2]))
+
+# calculating the average value of the ratings
+avg = sum(dt_list)/len(dt_list)        
+print("This is the average rating of the movies : " + str(avg))
