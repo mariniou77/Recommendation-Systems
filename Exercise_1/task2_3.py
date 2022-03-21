68fb83a333e122009b3c5919de824b004034648f
mariniou77 <antonis.marinidis7@gmail.com>
2022-03-21 10:55:38 +0100

    task2_3 without the mode in the function yet

--- Exercise_1/task2_3.py
+++ Exercise_1/task2_3.py
@@ -2,7 +2,7 @@
 from io import UnsupportedOperation
 
 # function to calculate the mean rating
-def computeMeanRating(name):
+def computeMean_Median_ModeRating(name):
     # handle errors
     try:
         with open(name) as d:
@@ -23,9 +23,24 @@ def computeMeanRating(name):
             dt_list.append(float(rating[2]))
 
     # calculating the average value of the ratings
-    return sum(dt_list)/len(dt_list)     
+    mean = sum(dt_list)/len(dt_list)  
+
+    # calculate the median value of the ratings
+    # devide the list in 2 parts and store it as an integer (//)
+    index = len(dt_list) // 2
+    # check if the length of the list is odd or even
+    # odd :
+    if len(dt_list) % 2:
+        median = sorted(dt_list)[index]
+    # even :    
+    else:
+        # find the sum of the 2 numbers in the middle of the sorted list and devide them by 2
+        median = sum(sorted(dt_list)[index -1:index + 1]) / 2    
+
+    return mean, median      
 
 # pain programm that calls the function with the name of the file 
 dataset = "Exercise_1/datasets/ratings.csv"    
-avg = computeMeanRating(dataset)  
-print("This is the average rating of the movies : " + str(avg))  
+avg, med = computeMean_Median_ModeRating(dataset)  
+print("This is the average rating of the movies : " + str(avg)) 
+print("This is the median rating of the movies : " + str(med)) 
