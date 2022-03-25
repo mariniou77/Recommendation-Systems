9bc2c263ad604d1dc435c216ed963d8faf389020
mariniou77 <antonis.marinidis7@gmail.com>
2022-03-25 12:41:22 +0100

    Create task2_2.py

---
+++ Exercise_2/task2_2.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+data = [['Toy Story',21.946943],['Jumanji',17.015539],['Grumpier Old Men',11.7129]]
+
+# Create a DataFrame object from the nested list with column headings ‘title’ and ‘popularity’
+dtf = pd.DataFrame(data, columns=('title', 'popularity'))
+print(dtf)
+print("\n\n")
+
+# Create a new DataFrame which has the entries sorted by popularity in ascending order
+dtf1 = dtf.sort_values("popularity", ascending=True)
+print(dtf1)
+print("\n\n")
+
+# Print the popularity values (with double [[]] shows also the column name)
+print(dtf1[['popularity']])
